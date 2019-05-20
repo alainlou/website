8fdcfd57192fc3b1070cc8ae76d28bd23beb9b42
alainlou <az2lou@uwaterloo.ca>
2019-05-19 21:35:32 -0400

    add plugin depedencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,17 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Alain Lou',
+    author: 'Alain Lou'
+  },
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark'
   ]
 }
