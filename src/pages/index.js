4f1c3ab8e602d8a7698ba7fca9579270378110e9
alainlou <az2lou@uwaterloo.ca>
2019-05-21 01:55:43 -0400

    add 404 and Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import SEO from '../components/SEO';
 
 const IndexPage = () => (
     <Layout>
+        <SEO title="Home"/>
         <h3>Hi! I'm Alain, a computer engineering student at the University of Waterloo.</h3>
     </Layout>
 );
