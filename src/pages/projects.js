b70b2be4b768097361ebf1823422f5c35b6388b9
alainlou <az2lou@uwaterloo.ca>
2019-05-21 06:03:53 -0400

    testing

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/SEO';
+import Card from '../components/card';
 
 const ProjectsPage = () => {
     return (
         <Layout>
             <SEO title="Projects"/>
             <p>This is a migration project from Github...</p>
+            <Card imgSrc="https://source.unsplash.com/user/erondu/600x400" title="Hello" content="This is a card"/>
         </Layout>
     );
 };
