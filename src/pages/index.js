db64ce4843c47075e35744324e79aeb0a4253b1f
alainlou <az2lou@uwaterloo.ca>
2019-05-21 05:13:45 -0400

    clarification

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
         <SEO title="Home"/>
         <h3>Hi!</h3>
         <p>
-            I'm Alain, a computer engineering student at the University of Waterloo. Over my degree, I'll have 6 co-op terms, which I hope to responsibly use to explore the tech industry and find out where I fit!
+            I'm Alain, a computer engineering student at the University of Waterloo. Over my undergraduate degree, I'll have 6 co-op terms, which I hope to responsibly use to explore the tech industry and find out where I fit!
         </p>
     </Layout>
 );
