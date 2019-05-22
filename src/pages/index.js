f484618fc02ce2edf348c4bc6723d35b24d43b4b
alainlou <az2lou@uwaterloo.ca>
2019-05-21 23:52:23 -0400

    Spelling error :(

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
         <SEO title="Home"/>
         <h3>Hey!</h3>
         <p>
-            I'm Alain, a computer engineering student at the University of Waterloo. Over my undergraduate career, I hope to use to explore the tech industry and find out where I fit!
+            I'm Alain, a computer engineering student at the University of Waterloo. Over my undergraduate career, I hope to explore the tech industry and find out where I fit!
         </p>
     </Layout>
 );
