4eefbca3d28d2a0fda2d855dff8f7763820cd3bd
alainlou <az2lou@uwaterloo.ca>
2019-05-21 23:17:17 -0400

    update language for the landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ import SEO from '../components/SEO';
 const IndexPage = () => (
     <Layout>
         <SEO title="Home"/>
-        <h3>Hi!</h3>
+        <h3>Hey!</h3>
         <p>
-            I'm Alain, a computer engineering student at the University of Waterloo. Over my undergraduate degree, I'll have 6 co-op terms, which I hope to use to explore the tech industry and find out where I fit!
+            I'm Alain, a computer engineering student at the University of Waterloo. Over my undergraduate career, I hope to use to explore the tech industry and find out where I fit!
         </p>
     </Layout>
 );
