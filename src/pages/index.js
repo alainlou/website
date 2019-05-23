4023bf2845d34797a0bf25ed4c02c70d9755ac2e
alainlou <az2lou@uwaterloo.ca>
2019-05-22 21:49:19 -0400

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,14 @@ const IndexPage = () => (
     <Layout>
         <SEO title="Home"/>
         <h3>Hey!</h3>
+        <br></br>
         <p>
-            I'm Alain, a computer engineering student at the University of Waterloo. Over my undergraduate career, I hope to explore the tech industry and find out where I fit!
+            I'm Alain, a computer engineering student at the University of Waterloo. 
+            I'm interested in applying my knowledge to tackle big problems.
+            Over my undergraduate career, I hope to explore the tech industry and find out where I fit!
+        </p>
+        <p>
+            This is my little corner on the internet where I talk about my experiences.
         </p>
     </Layout>
 );
