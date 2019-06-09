709bef6e79fde1df4f1202dacf05f013cffbd1b3
alainlou <az2lou@uwaterloo.ca>
2019-06-09 11:02:11 -0400

    minimal blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,12 +2,41 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/SEO';
+import { useStaticQuery } from 'gatsby';
 
-const BlogPage = () => (
-    <Layout>
-        <SEO title="Blog"/>
-        <p>I promise I will start blogging soon! First post should be out by the middle of June.</p>
-    </Layout>
-);
+const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                    frontmatter {
+                        title
+                        date
+                    }
+                    html
+                    excerpt
+                    }
+                }
+            }
+        }
+    `)
+    return (
+        <Layout>
+            <SEO title="Blog"/>
+            <div>
+                {data.allMarkdownRemark.edges.map((edge, i) => {
+                    return (
+                        <div>
+                            <h2>{edge.node.frontmatter.title}</h2>
+                            <h6>{edge.node.frontmatter.date}</h6>
+                            <p>{edge.node.excerpt}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </Layout>
+    );
+};
 
 export default BlogPage;
