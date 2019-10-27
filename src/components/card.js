385932bbf70dea776ab885705f6bfaccc1a97e2e
alainlou <az2lou@uwaterloo.ca>
2019-10-27 15:21:42 -0400

    blog pipeline is complete

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Img from 'gatsby-image';
 
+import commonStyles from '../styles/common.module.scss';
 import cardStyles from './card.module.scss';
 
-const CardBody = ({title, content, url}) => {
+const CardBody = ({title, content}) => {
     return (
         <div className={cardStyles.cardBody}>
             <h3>{title}</h3>
@@ -17,9 +18,9 @@ const CardBody = ({title, content, url}) => {
 const Card = ({title, fluid, url, content}) => {
     return (   
         <article className={cardStyles.card}>
-            <a className={cardStyles.plain} href={url} target="_blank" rel="noopener noreferrer">
+            <a className={commonStyles.plain} href={url} target="_blank" rel="noopener noreferrer">
                 <Img className={cardStyles.cardHeader} fluid={fluid}/>
-                <CardBody title={title} content={content} url={url}/>
+                <CardBody title={title} content={content}/>
             </a>
         </article>
     );
--- static/blog/introduction.md
+++
@@ -1,7 +0,0 @@
----
-title: "Introduction"
-author: "Alain Lou"
-date: "2019-05-18"
----
-
-This is my blog. I'll be writing about my life experiences here as a sort-of "public personal diary".
